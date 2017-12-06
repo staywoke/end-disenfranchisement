928d0f6a3176e83f8112d59d9d0316b60fb5c16d
Peter Schmalfeldt <me@peterschmalfeldt.com>
2017-12-05 23:07:14 -0500

    Update IPN

--- ipn.php
+++ ipn.php
@@ -67,9 +67,6 @@ if($ipnMessage->validate()) {
 
     // Calculate Number of Mailers
     $cost_per_mailer = floatval(COST_PER_MAILER);
-    $mc_currency = floatval($mc_currency);
-    $mc_fee = floatval($mc_fee);
-    // $total = ($mc_gross - $mc_fee); -- was instructed to diregard paypal fees
     $total = floatval($mc_gross);
 
     // "Flip a Coin" to see if we should round up or down
