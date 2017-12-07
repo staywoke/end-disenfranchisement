effbcc1c367982b4b85c007035af13c9eea7c660
Peter Schmalfeldt <me@peterschmalfeldt.com>
2017-12-07 18:15:11 -0500

    Remove Partner Section

--- index.php
+++ index.php
@@ -268,6 +268,18 @@ require 'config.php';
 
     </section> <!-- end .cta-section-3  -->
 
+    <!-- ABOUT US -->
+    <a name="about-us"></a>
+    <section class="cta-section-3 alt-row">
+      <div class="container">
+        <div class="about-us">
+          <p>
+            This platform, a project of <a href="http://staywoke.org" target="_blank">StayWoke</a>, was built in partnership with the <a href="https://floridarrc.com/" target="_blank">Florida Rights Restoration Coalition</a> to collect the petition signatures to put voter rights restoration on the 2018 Florida ballot. With assistance from <a href="https://www.rockthevote.org/" target="_blank">Rock The Vote</a>, we’ve built a crowdsourced system that mails petitions, including prepaid return postage, directly to thousands of registered voters in the state at minimal cost.
+          </p>
+        </div>
+      </div>
+    </section>
+
     <!-- FOOTER -->
     <section class="fixed-footer">
       <div class="row social-share">
