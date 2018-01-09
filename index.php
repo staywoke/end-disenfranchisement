512e7ba4752e15ab88b7745645f3a8603fedbd9d
Peter Schmalfeldt <me@peterschmalfeldt.com>
2018-01-09 12:44:25 -0500

    Make Shipping Required

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ if (MAINTENANCE_MODE && !isset($_REQUEST['skip_redirect'])) {
           <input type="hidden" name="item_name" value="StayWoke">
           <input type="hidden" name="item_number" value="FL">
           <input type="hidden" name="no_note" value="1">
+          <input type="hidden" name="no_shipping" value="2">
           <input type="hidden" name="rm" value="1">
           <input type="hidden" name="currency_code" value="USD">
           <input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
